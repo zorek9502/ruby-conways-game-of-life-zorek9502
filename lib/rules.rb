df10150c6b31426dc71e01a1721188313fa64405
zorek9502 <zorek_ltc@hotmail.com>
2019-04-01 19:13:02 -0600

    Create a universe and count neighbors
    Creates a matrix of nxn and fills it with random values between 1 and 0
    Function that counts how many live neighbors are around an specific cell
    Added a couple tests for the function that counts neighbors

--- lib/rules.rb
+++ lib/rules.rb
@@ -5,5 +5,22 @@ require "Matrix"
 
 class GoL_Rules
   def initialize
+    @universe
+  end
+
+  def create_universe(cols, rows)
+    @universe = Matrix.build(row, cols) { rand(2) }
+    #@universe = Matrix[["a", "b", "c", "d"], ["e", "f", "g", "h"], ["i", "j", "k", "l"], ["m", "n", "o", "p"]]
+  end
+
+  #Funcion para contar los vecinos vivos
+  def count_neighbors(universe, i, j)
+    sum = 0
+    (-1...2).each do |x|
+      (-1...2).each do |y|
+        sum += universe.component(x, y)
+      end
+    end
+    sum -= universe.component(i, j)
   end
 end
--- specs/rules.specs.rb
+++ specs/rules.specs.rb
@@ -3,4 +3,19 @@ require "minitest/autorun"
 require "Matrix"
 
 class Game_of_life < Minitest::Test
+  def setup
+    @gol = GoL_Rules.new
+  end
+
+  def test_count_neighbors()
+    universe = Matrix[[1, 0, 1, 0], [0, 0, 0, 1], [0, 1, 0, 0], [0, 0, 1, 1]]
+    resp = @gol.count_neighbors(universe, 0, 0)
+    assert_equal(resp, 2)
+  end
+
+  def test_count_neighbors2()
+    universe = Matrix[[0, 1, 0, 0], [0, 1, 0, 1], [1, 1, 1, 1], [0, 1, 0, 0]]
+    resp = @gol.count_neighbors(universe, 2, 3)
+    assert_equal(resp, 3)
+  end
 end
